2fb37e5f5b388af1e4e71b6df0f67151a0ceee88
Sebastian Moreno <sebycolo32@gmail.con>
2024-02-01 23:26:58 -0600

    Initial Commit

---
+++ TuneTally/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TuneTally
+//
+//  Created by Sebastian Moreno on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, cheby!")
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TuneTally/TuneTallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneTallyApp.swift
+//  TuneTally
+//
+//  Created by Sebastian Moreno on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TuneTallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
